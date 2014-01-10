bb71be6b6885fc0d9f7aa6b575050c352479ecab
Vlad Mazur <vlad@mazuru.com>
2014-01-11 01:34:23 +0200

    start working on project; some stuff for static using

--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
 #include <QCoreApplication>
 
+using namespace std;
+
+typedef enum {DAC=0, ADC=1} MODE;
+
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    list <string> files;
 
-    return a.exec();
+//    set files
+    files.push_back("Files//task.v");
+    files.push_back("Files//myoutfile.dat");
+    files.push_back("Files//reg.atd");
+    files.push_back("Files//allted.ato");
+
+//    set time and mode
+    unsigned time = 100;
+    MODE mode = DAC;
+
+    if (mode = DAC)
+    {
+//        set levels and times
+        double level_of_0 = 0.8, level_of_1 = 2.8, time_of_lead_edge = 3.0, time_of_trail_edge = 2.0;
+//        get matched signals
+
+    }
 }
